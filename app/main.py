5e6c2168747688103f7c92599533c97b360ef2de
EkaitzBlanca <ekaproo1@gmail.com>
2026-03-25 00:48:05 +0100

    cors

--- app/main.py
+++ app/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import user_router
 
 app = FastAPI()
+
+app.add_middleware(
+	CORSMiddleware,
+	allow_origins=[
+		"http://localhost:4200",
+        "https://kardoria-webapp-backend.onrender.com"
+	],
+	allow_credentials=True,
+	allow_methods=["*"],
+	allow_headers=["*"],
+)
 
 app.include_router(user_router.router)
 

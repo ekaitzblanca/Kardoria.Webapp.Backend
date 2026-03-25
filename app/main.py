cf3972b719996bdf46cdd323a595629df6fb9c95
EkaitzBlanca <ekaproo1@gmail.com>
2026-03-25 01:14:50 +0100

    prueba cors

--- app/main.py
+++ app/main.py
@@ -8,8 +8,10 @@ app.add_middleware(
 	CORSMiddleware,
 	allow_origins=[
 		"http://localhost:4200",
-        "https://kardoria.netlify.app/"
+		"https://kardoria.netlify.app",
+		"https://www.kardoria.netlify.app",
 	],
+	allow_origin_regex=r"https://.*\.netlify\.app",
 	allow_credentials=True,
 	allow_methods=["*"],
 	allow_headers=["*"],
